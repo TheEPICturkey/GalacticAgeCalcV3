bade0c797b271266bbc58ce5f145fb112727c622
Brandon Spear <BowDownToTheEpicTurkey@gmail.com>
2022-10-11 07:33:23 -0700

    it will do test#1.

--- __tests__/age.tests.js
+++ __tests__/age.tests.js
@@ -1,1 +1,8 @@
 import { EarthAge, roundTwoDecimal } from './../src/EarthAge.js'
+
+describe ('EarthAge', () => {
+
+  test("display the users Age on Earth as 25", () => {
+    const userAge = new EarthAge(25);
+    expect(userAge.Age).toEqual(25);
+  });
